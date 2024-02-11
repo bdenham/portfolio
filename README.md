e42ae9fb4eed1fe95ec918c19facaaecd9515a73
Bruce Denham <bdenham@adobe.com>
2024-02-11 14:42:42 -0600

    fix: update site with template files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# portfolio
-My portfolio
+# My Portfolio
+
+This is my portfolio, where I will showcase my projects and skills.
---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// Example: A cheatsheet of many common Zod datatypes
+import { z, defineCollection } from 'astro:content';
+
+const postsCollections = defineCollection({
+  schema: ({ image }) => z.object({ 
+    isDraft: z.boolean(),
+    title: z.string(),
+    description: z.string().optional(),
+    image: image(),
+    author: z.string().default('Bruce'),
+    tags: z.array(z.string()).optional(),
+    categories: z.array(z.string()).default('uncategorized'),
+    publishDate: z.string(),
+  })
+})
+export const collections = {
+  posts: postsCollections,
+};
